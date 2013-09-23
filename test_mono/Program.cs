9146427f99e0dfd7890c8ec4c12751dfa7e5c003
ilya.cherkasov <ilya.cherkasov@teligent.ru>
2013-09-23 23:07:48 +0400

    Async delegates remade

--- test_mono/Program.cs
+++ test_mono/Program.cs
@@ -4,91 +4,109 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Diagnostics;
+using System.Linq;
 
 namespace test_mono
 {
+	struct GameAction
+	{
+		public GameType GameType;
+		public Action<GameType> Delegate;
+		public IAsyncResult Result;
+	}
+
+	enum GameType
+	{
+		Redux = 0,
+		SpectralGames = 1,
+		NormalForms = 2,
+		KNucleotide = 3,
+		RegexDna = 4,
+	}
+
 	class MainClass
 	{
-		static readonly Random Rand = new Random();
-
+		static readonly Random Rand = new Random ();
 		const int TASK_COOLDOWN = 700;
 		const int TASK_COOLDOWN_DELTA = 200;
-		
-		enum GameType {
-			Redux = 0,
-			SpectralGames = 1,
-			NormalForms = 2,
-			KNucleotide = 3,
-			RegexDna = 4,
-		}
-		
-		static readonly Dictionary<GameType, string> ClassPath = new Dictionary<GameType, string>{
-			{GameType.Redux, "domain_dll.Redux.Game"},
-			{GameType.SpectralGames, "domain_dll.SpectralNorms.Game"},
-			{GameType.NormalForms, "domain_dll.NGame.Game"},
-			{GameType.KNucleotide, "domain_dll.nukleotide.Game"},
-			{GameType.RegexDna, "domain_dll.RegexDna.Game"},
+		static readonly Dictionary<GameType, string> ClassPath = new Dictionary<GameType, string> {
+ { GameType.Redux, "domain_dll.Redux.Game" },
+ { GameType.SpectralGames, "domain_dll.SpectralNorms.Game" },
+ { GameType.NormalForms, "domain_dll.NGame.Game" },
+ { GameType.KNucleotide, "domain_dll.nukleotide.Game" },
+ { GameType.RegexDna, "domain_dll.RegexDna.Game" },
 		};
-		
-		public static void Main(string[] args)
+		static readonly Queue<GameAction> Games = new Queue<GameAction> (Environment.ProcessorCount);
+
+		public static void Main (string[] args)
 		{
 			var cooldown = TASK_COOLDOWN;
 			if (args.Length > 0) {
-				cooldown = int.Parse(args[0]);
-				Console.WriteLine("Cooldown is {0} ms", cooldown);
+				cooldown = int.Parse (args [0]);
+				Console.WriteLine ("Cooldown is {0} ms", cooldown);
 			}
-			Console.WriteLine("Starting...");
-			while (true)
-			{ // catch ctrl+c
-				GameType type;
-				GetRandomTask(out type);
-				ThreadPool.QueueUserWorkItem(new WaitCallback((object state) =>
-				{
-					try 
-					{
-						RunThreadPoolTask(type);
+			while (true) { // catch ctrl+c
+				if (Games.Count < Environment.ProcessorCount) {
+					GameType type;
+					GetRandomTask (out type);
+					Console.WriteLine ("Enqueing type " + type);
+					var action = new Action<GameType> (RunGame);
+					Games.Enqueue (new GameAction () { 
+						GameType = type,
+						Delegate = action, 
+						Result = action.BeginInvoke (type, null, null) // run game here
+					});
+				} else {
+					var game = Games.Dequeue ();
+					Console.WriteLine ("Waiting for {0} to complete", game.GameType);
+					try {
+						game.Delegate.EndInvoke (game.Result); // sync here
+					} catch (Exception ex) {
+						Console.WriteLine ("Got exception here, okay i guess... " + ex.GetType ());
+						Console.WriteLine (ex);
 					}
-					catch (Exception ex) 
-					{
-						Console.WriteLine("Okay, exception! {0}", ex.GetType());
-					}
-				}));
-				Thread.Sleep(cooldown - (int)(GetRandDouble() * TASK_COOLDOWN_DELTA));
+				}
 			}
 		}
 
-		static double GetRandDouble()
+		static void RunGame (GameType type)
 		{
-			return Rand.NextDouble();
+			try {
+				RunThreadPoolTask (type);
+			} catch (Exception ex) {
+				Console.WriteLine ("Exception! Too bad");
+			}
 		}
 
-		static void GetRandomTask(out GameType type)
+		static double GetRandDouble ()
 		{
-			var randVal = Rand.Next(0, 5);
+			return Rand.NextDouble ();
+		}
+
+		static void GetRandomTask (out GameType type)
+		{
+			var randVal = Rand.Next (1, 5); // no redux
 			type = (GameType)randVal;
 		}
 
-		static object GetRandomInt(int min, int max)
+		static object GetRandomInt (int min, int max)
 		{
-			return Rand.Next(min, max);
+			return Rand.Next (min, max);
 		}
 
-		static void RunThreadPoolTask(GameType type)
+		static void RunThreadPoolTask (GameType type)
 		{
-			Console.WriteLine("Starting game `{0}`...", type);
-			var className = ClassPath[type];
-			Stopwatch sw = Stopwatch.StartNew();;
-			AppDomain domain = AppDomain.CreateDomain("Domain #" + DateTime.Now.Ticks);
-			try 
-			{
-				var instance = domain.CreateInstanceAndUnwrap("domain_dll", className);
-				var method = instance.GetType().GetMethod("Run");
-				method.Invoke(instance, new [] {GetRandomInt(10, 20)});
-			}
-			finally {
-				AppDomain.Unload(domain);
-				Console.WriteLine("Done game {0}. Elapsed = {1} s.", type, sw.Elapsed.TotalSeconds);
-				sw.Stop();
+			var className = ClassPath [type];
+			var sw = Stopwatch.StartNew ();
+			var domain = AppDomain.CreateDomain ("Domain #" + DateTime.Now.Ticks);
+			try {
+				var instance = domain.CreateInstanceAndUnwrap ("domain_dll", className);
+				var method = instance.GetType ().GetMethod ("Run");
+				method.Invoke (instance, new [] { GetRandomInt (10, 20) });
+			} finally {
+				AppDomain.Unload (domain);
+				Console.WriteLine ("Done game {0}. Elapsed = {1} s.", type, sw.Elapsed.TotalSeconds);
+				sw.Stop ();
 			}
 		}
 	}
